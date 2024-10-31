97ef03ac1491aa91d8ca972ff83d54db7891de6d
mpedus <pmikhailmail@gmail.com>
2024-10-31 22:50:07 +0000

    Fix pubs location

--- pubs.py
+++ pubs.py
@@ -8,20 +8,16 @@ Created on Thu Oct 31 22:27:20 2024
 import folium
 import pandas as pd
 
-# Загрузка данных из файла
 data = pd.read_csv("C://pubs/pubs.csv")
 
-# Определяем центр карты для Лукана, Ирландия
 center_lat, center_lon = 53.3574, -6.4481
 city_map = folium.Map(location=[center_lat, center_lon], zoom_start=13)
 
-# Добавляем бары на карту
 for _, row in data.iterrows():
     folium.Marker(
         location=[row['latitude'], row['longitude']],
-        popup=f"{row['name']} - Посещений: {row['visits']}",
+        popup=f"{row['name']} - visits: {row['visits']}",
         icon=folium.Icon(color="blue" if row['visits'] == 0 else "red")
     ).add_to(city_map)
 
-# Сохраняем карту в HTML-файл
 city_map.save("C://pubs/city_bars_map.html")
